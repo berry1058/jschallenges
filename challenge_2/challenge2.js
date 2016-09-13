5f1a6ef95563c6bb70d880d5dca3372c81c2cb91
roland <roland.cuffee@yahoo.com>
2016-09-13 16:42:45 -0400

    Challenge2 commit

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,26 @@
 $(function(){
 
-	//all your code should go here
+   var replaced = $("body").html().replace(/Bacon/g,'Laser Vision');
+        $("body").html(replaced);    
 
-})
+        var state = true;
+	   $("h1").click(function(){
+	       if (state){
+           $("h1").css({color: "red"});
+       }else{
+           $("h1").css({color: "black"});
+       }
+
+       state = !state;
+
+    });    
+	   $('.title:even').hide();  
+
+	   $('.post:last').remove();
+       $('.post:last').remove();
+       $('.post:last').remove(); 
+
+       $('aside').remove();
+
+       $('footer li:even').hide();
+   });
